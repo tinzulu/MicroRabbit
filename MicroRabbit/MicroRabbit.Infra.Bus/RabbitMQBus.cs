fdf83b9c9b876315aa932f65fc7de311f677a535
Tinashe Zulu <tinzulu@gmail.com>
2020-04-27 22:03:27 +0200

    Added RabbitMQ infrastructure

---
+++ MicroRabbit/MicroRabbit.Infra.Bus/RabbitMQBus.cs
@@ -0,0 +1,30 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Infra.Bus
+{
+    public sealed class RabbitMQBus : IEventBus
+    {
+        public void Publish<T>(T @event) where T : Event
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
